ea251250d6af20ee1dceb2be97bb456364f1ba41
vishnu1909 <113003675+vishnu1909@users.noreply.github.com>
2023-02-06 21:29:33 +0530

    Add files via upload

---
+++ 2.copy of files.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE *sourceFile;
+    FILE *destFile;
+    char sourcePath[100];
+    char destPath[100];
+    char ch;
+    printf("Enter source file path: ");
+    scanf("%s", sourcePath);
+    printf("Enter destination file path: ");
+    scanf("%s", destPath);
+    sourceFile  = fopen(sourcePath, "r");
+    destFile    = fopen(destPath,   "w");
+    if (sourceFile == NULL || destFile == NULL)
+    {
+        printf("\nUnable to open file.\n");
+        printf("Please check if file exists and you have read/write privilege.\n");
+        exit(EXIT_FAILURE);
+    }
+    ch = fgetc(sourceFile);
+    while (ch != EOF)
+    {
+        fputc(ch, destFile);
+        ch = fgetc(sourceFile);
+    }
+    printf("\nFiles copied successfully.\n");
+    fclose(sourceFile);
+    fclose(destFile);
+	return 0;
+}
